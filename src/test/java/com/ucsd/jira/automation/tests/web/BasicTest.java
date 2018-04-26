372af248bd2bae7aa4c9df470210350bf78c3234
ctagariello <ctag001@gmail.com>
2018-04-25 19:52:14 -0700

    Added Assertfail and AssertContains.

--- src/test/java/com/ucsd/jira/automation/tests/web/BasicTest.java
+++ src/test/java/com/ucsd/jira/automation/tests/web/BasicTest.java
@@ -3,17 +3,23 @@ package com.ucsd.jira.automation.tests.web;
 import com.pwc.core.framework.annotations.Issue;
 import com.pwc.core.framework.annotations.MaxRetryCount;
 import com.pwc.core.framework.listeners.Retry;
+import com.sun.tools.javac.util.Assert;
 import com.ucsd.jira.automation.data.Constants;
 import com.ucsd.jira.automation.data.enums.LeftMenu;
 import com.ucsd.jira.automation.frameworksupport.Groups;
 import com.ucsd.jira.automation.frameworksupport.JiraTestCase;
 import org.testng.annotations.Test;
 
+import static com.pwc.assertion.AssertService.assertContains;
 import static com.pwc.logging.service.LoggerService.FEATURE;
 import static com.pwc.logging.service.LoggerService.GIVEN;
 import static com.pwc.logging.service.LoggerService.SCENARIO;
 import static com.pwc.logging.service.LoggerService.THEN;
 import static com.pwc.logging.service.LoggerService.WHEN;
+
+
+import static com.pwc.assertion.AssertService.assertFail;
+import static com.pwc.logging.service.LoggerService.LOG;
 
 public class BasicTest extends JiraTestCase {
 
@@ -48,7 +54,8 @@ public class BasicTest extends JiraTestCase {
 
         webAction(LeftMenu.PROJECTS);
         webElementVisible(combine(Constants.VARIABLE_BY_TEXT_HEADING, "Projects"));
-
+        //assertFail("this is a test",Constants.ADVANCED_SEARCH_TEXTAREA, "NOT UCSD");
+        assertContains("this is a test",Constants.ADVANCED_SEARCH_TEXTAREA, "NOT UCSD");
     }
 
 }
