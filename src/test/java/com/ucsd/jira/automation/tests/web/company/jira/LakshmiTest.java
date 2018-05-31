40de4cdac23529fb22d554315db51721b212ffbb
Lakshmi Iyer <38305901+lakshmiiyerp@users.noreply.github.com>
2018-05-30 21:28:21 -0700

    Story lakshmi (#23)
    * STORY-LAKSHMI: add a new test for editing users
    
    * rename LAKSHMITEST to LakshmiTest
    changed testBasic to testLakshmi
    
    * add a comment
    
    * moving lakshmitest to the right package

--- src/test/java/com/ucsd/jira/automation/tests/web/company/jira/LakshmiTest.java
+++ src/test/java/com/ucsd/jira/automation/tests/web/company/jira/LakshmiTest.java
@@ -1,4 +1,4 @@
-package com.ucsd.jira.automation.tests.web;
+package com.ucsd.jira.automation.tests.web.company.jira;
 
 import com.pwc.core.framework.annotations.Issue;
 import com.pwc.core.framework.listeners.Retry;
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 
 import static com.pwc.logging.service.LoggerService.*;
 
-
+//Test Automation File
 public class LakshmiTest extends JiraTestCase {
 
     @Override
