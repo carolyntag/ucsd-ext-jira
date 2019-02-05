76f7c270f4c2216923c5fc352ab9765a2a5074b1
c_anlomb <c_anlomb@qualcomm.com>
2019-02-04 19:02:36 -0800

    Examples

--- src/test/java/com/ucsd/jira/automation/tests/web/company/jira/ImplicitNavigationTest.java
+++ src/test/java/com/ucsd/jira/automation/tests/web/company/jira/ImplicitNavigationTest.java
@@ -5,8 +5,6 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
