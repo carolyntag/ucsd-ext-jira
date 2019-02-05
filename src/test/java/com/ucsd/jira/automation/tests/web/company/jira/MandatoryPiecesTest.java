641350599e075781f9305a9042f8818482b9e6a0
c_anlomb <c_anlomb@qualcomm.com>
2019-02-04 19:04:17 -0800

    Examples

---
+++ src/test/java/com/ucsd/jira/automation/tests/web/company/jira/MandatoryPiecesTest.java
@@ -0,0 +1,43 @@
+package com.ucsd.jira.automation.tests.web.company.jira;
+
+import com.ucsd.jira.automation.frameworksupport.Groups;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class MandatoryPiecesTest {
+
+    @BeforeTest
+    public static void setUp() {
+
+    }
+
+    @AfterTest
+    public static void tearDown() {
+
+    }
+
+    @Test(groups = {Groups.ACCEPTANCE_TEST})
+    public void testMandatoryPieces() {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\workspace\\ucsd-ext-jira\\target\\test-classes\\drivers\\chrome\\chrome_win.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://ucsd-ext.atlassian.net");
+        driver.manage().window().maximize();
+
+        Assert.assertEquals("a", "a");
+
+        driver.quit();
+
+    }
+
+
+}
+
